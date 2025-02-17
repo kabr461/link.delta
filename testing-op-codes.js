c8b78d1081d39d1c9cc65f43c39d0e066ad2bd7b
kabr461 <kabirmangi761@gmail.com>
2025-02-17 18:14:12 +0500

    Update testing-op-codes.js

--- testing-op-codes.js
+++ testing-op-codes.js
@@ -1,7 +1,7 @@
 (function () {
     const messages = [];
     const opcodeStats = {};
-
+    
     function hexFormat(buffer) {
         return [...new Uint8Array(buffer)]
             .map(b => b.toString(16).padStart(2, "0"))
@@ -9,6 +9,8 @@
     }
 
     function logMessage(type, data) {
+        if (!(data instanceof ArrayBuffer)) return; // Ensure it's binary data
+
         const timestamp = Date.now();
         const hexData = hexFormat(data);
         const opcode = hexData.split(" ")[0] || "unknown";
@@ -22,16 +24,27 @@
         opcodeStats[opcode].times.push(timestamp);
     }
 
-    WebSocket.prototype._send = WebSocket.prototype.send;
-    WebSocket.prototype.send = function (data) {
-        logMessage("send", data);
-        return this._send(data);
+    const openWebSockets = new Set();
+
+    const OriginalWebSocket = WebSocket;
+    window.WebSocket = function (...args) {
+        const ws = new OriginalWebSocket(...args);
+        openWebSockets.add(ws);
+
+        ws.addEventListener("message", (event) => {
+            if (event.data instanceof ArrayBuffer) {
+                logMessage("receive", event.data);
+            }
+        });
+
+        return ws;
     };
 
-    WebSocket.prototype._onmessage = WebSocket.prototype.onmessage;
-    WebSocket.prototype.onmessage = function (event) {
-        logMessage("receive", event.data);
-        return this._onmessage ? this._onmessage(event) : null;
+    WebSocket.prototype.send = function (data) {
+        if (data instanceof ArrayBuffer) {
+            logMessage("send", data);
+        }
+        return OriginalWebSocket.prototype.send.apply(this, arguments);
     };
 
     setTimeout(() => {
@@ -43,5 +56,5 @@
         a.click();
     }, 30000);
 
-    console.log("WebSocket logging started for 30 seconds...");
+    console.log("✅ WebSocket logging started for 30 seconds...");
 })();
